a9422c4b048d35d43d8b8024aa9ccccca953dfcb
yhl <1004655482@qq.com>
2021-11-02 15:39:45 +0800

    算法积累，进行工具类集合

---
+++ src/main/java/com/example/algorithm/util/Test.java
@@ -0,0 +1,16 @@
+package com.example.algorithm.util;
+
+/**
+ * Test描述
+ *
+ * @author Administrator
+ * @date 2021-11-1
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(Math.abs(-2147483648));
+        //System.out.println(1/0);
+        System.out.println(1.0/0.0);
+        System.out.println(Math.log(Math.E));
+    }
+}
